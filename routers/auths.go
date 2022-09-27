0a518ff8404ffb2c4220e510da2c9105b6f58e9d
lucifer008 <zhangxiaolin198313@163.com>
2022-09-27 10:30:55 +0800

    >>>> add 结构

--- routers/auths.go
+++ routers/auths.go
@@ -2,11 +2,21 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"zld-gly/models"
 )
 
 func addAuthsRouters(rg *gin.RouterGroup) {
 	rg.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, "auths")
+	})
+	rg.POST("/login", func(context *gin.Context) {
+		var user models.Users
+		if err := context.BindJSON(&user); err != nil {
+			log.Printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", user.Username)
+			log.Default().Fatal(err)
+		}
+		log.Printf(user.Password)
 	})
 }
