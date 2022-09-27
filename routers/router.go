0c400195347eaace75f334de21ebd9788acc8eb4
lucifer008 <zhangxiaolin198313@163.com>
2022-09-27 09:32:51 +0800

    >>>> add 结构

--- routers/router.go
+++ routers/router.go
@@ -12,5 +12,10 @@ func Run() {
 	router.Run(":5000")
 }
 func getRoutes() {
-
+	user := router.Group("/user")
+	order := router.Group("/orders")
+	customer := router.Group("customer")
+	addUserRouters(user)
+	addOrderRouters(order)
+	addCustomerRouters(customer)
 }
--- routers/users.go
+++ routers/users.go
@@ -1,1 +1,22 @@
 package routers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func addUserRouters(rg *gin.RouterGroup) {
+	rg.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, "users")
+	})
+}
+func addOrderRouters(rg *gin.RouterGroup) {
+	rg.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, "orders")
+	})
+}
+func addCustomerRouters(rg *gin.RouterGroup) {
+	rg.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, "customer")
+	})
+}
