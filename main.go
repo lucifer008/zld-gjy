37dd00303d65f2991109545688f0c8f9adc6a88e
zhangxiaolin198313@163.com <zhangxiaolin198313@163.com>
2022-09-29 21:29:26 +0800

    >>>>>dao

--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 	"golang.org/x/net/context"
-	"gorm.io/gen/examples/dal"
 	"zld-jy/conf"
+	"zld-jy/da/base"
 	"zld-jy/da/query"
 	"zld-jy/routers"
 )
 
 func init() {
-	dal.DB = dal.ConnectDB(conf.MySQLDSN).Debug()
-	userDao := query.Use(dal.DB).SysUser
+	base.DB = base.ConnectDB(conf.MySQLDSN).Debug()
+	userDao := query.Use(base.DB).SysUser
 	user, err := userDao.WithContext(context.Background()).First()
 	if err != nil {
 		fmt.Printf("错误", err)
