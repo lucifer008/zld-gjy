b6324f7ce273ad754a7b7fa956c3709b93de462b
lucifer008 <zhangxiaolin198313@163.com>
2022-09-30 10:26:57 +0800

    >>>>unit test

--- tests/company.dao_test.go
+++ tests/company.dao_test.go
@@ -1,12 +1,14 @@
 package tests_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 	"zld-jy/da/model"
 	"zld-jy/da/query"
 )
 
+//测试create
 func Test_newCompany(t *testing.T) {
 	useOnce.Do(CRUDInit)
 
@@ -26,4 +28,34 @@ func Test_newCompany(t *testing.T) {
 	if err != nil {
 		t.Errorf("create model fail: %s", err)
 	}
+}
+
+//测试事务
+func Test_Trans(t *testing.T) {
+	qur := query.Use(DB)
+	err := qur.Transaction(func(tx *query.Query) error {
+		id, _ := tx.Company.WithContext(ctx).Count()
+		ids := id + 1
+		fmt.Println("插入Id", ids)
+		tx.Company.WithContext(ctx).Create(&model.Company{
+			ID:          id + 1,
+			CompanyName: "测试", RegisterDateTime: time.Now(),
+			InsertUser:     1,
+			InsertDateTime: time.Now(),
+			UpdateUser:     1,
+			UpdateDateTime: time.Now(),
+			Version:        1,
+			Deleted:        "0",
+		})
+		if ids > 1 {
+			panic("手动异常，测试回滚!")
+		}
+		tx.Company.WithContext(ctx).Delete(&model.Company{ID: id + 11})
+
+		return nil
+	})
+	if err != nil {
+		fmt.Errorf("错误:%w", err)
+	}
+	fmt.Println(">>>>>>>>>>>>>success!>>>>>>>>>>>>>>>>>>>>>>>>>")
 }
