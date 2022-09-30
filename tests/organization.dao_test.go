e9c418d4cad05d96f1afe062e98b238beb99f9a3
lucifer008 <zhangxiaolin198313@163.com>
2022-09-30 12:19:21 +0800

    >>>>unit test

--- tests/organization.dao_test.go
+++ tests/organization.dao_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"fmt"
 	"testing"
+	"time"
 	"zld-jy/da/model"
 	"zld-jy/da/query"
 )
@@ -11,9 +12,14 @@ func Test_Init_Org(t *testing.T) {
 	qur := query.Use(DB)
 	com, error := qur.Company.WithContext(ctx).First()
 	if error != nil {
-		fmt.Printf("%w", error)
+		fmt.Errorf("%w", error)
 		return
 	}
 
-	qur.Organization.WithContext(ctx).Create(&model.Organization{ID: 1, ComID: com.ID, OrgName: "zld", OrgCode: "zld-001", OrgLevel: 1})
+	err := qur.Organization.WithContext(ctx).Create(&model.Organization{ID: 1, ComID: com.ID, OrgName: "zld", OrgCode: "zld-001", OrgLevel: 1, BeginDate: time.Now(), EndDate: time.Now().AddDate(0, 1, 0), InsertUser: 1, InsertDateTime: time.Now(), UpdateUser: 1, UpdateDateTime: time.Now(), Version: 0, Deleted: "0"})
+	if err != nil {
+		fmt.Errorf("%w", err)
+		return
+	}
+	fmt.Println(">>>>>>>>>>>>>>>create org sucess!")
 }
