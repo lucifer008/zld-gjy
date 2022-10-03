2b2e8eaa2cd6b79a255fb160b365988f2efe4cc3
zhangxiaolin198313@163.com <zhangxiaolin198313@163.com>
2022-10-04 00:27:41 +0800

    >>>>>auths

--- tests/token_test.go
+++ tests/token_test.go
@@ -21,3 +21,24 @@ func Test_Token_Generator(t *testing.T) {
 	tokenString, err := token.SignedString(keyVal)
 	fmt.Println(tokenString, err)
 }
+func Test_Token_Check(t *testing.T) {
+	var tokenString = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYmYiOjE2NjU0MDMyMDAsInVzZXJJZCI6MTIxLCJ1c2VybmFtZSI6InpoYW5nc2FuIn0.rPyzZssFatQr5_ZTnVVTSV3WfE8_-5o0EXuii0niHBI"
+	//var hmacSampleSecret map[string]string
+	//密钥
+	var keyVal interface{}
+	keyVal = []byte("123456")
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		token.Valid = true
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return keyVal, nil
+	})
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		fmt.Println(claims["username"], claims["nbf"])
+	} else {
+		fmt.Println(err)
+	}
+}
