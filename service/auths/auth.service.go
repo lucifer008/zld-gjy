6c9cc0f8fc5131494e79c61028f491030beb1911
zhangxiaolin198313@163.com <zhangxiaolin198313@163.com>
2022-10-03 23:54:31 +0800

    >>>>>auths

--- service/auths/auth.service.go
+++ service/auths/auth.service.go
@@ -2,6 +2,9 @@ package auths
 
 import (
 	"context"
+	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"time"
 	"zld-jy/da/base"
 	"zld-jy/da/query"
 	"zld-jy/models"
@@ -31,6 +34,14 @@ func (impl *AuthServiceImpl) Auths(u models.Users) models.Auths {
 		return models.Auths{Status: 1, Desc: "无此用户信息"}
 	}
 	employee, _ := employeeDao.WithContext(context.Background()).Where(employeeDao.ID.Eq(userinfo.EmpID)).Take()
-
-	return models.Auths{Username: userinfo.UserName, EmplyeeName: employee.EmpName, AuthsToken: "2222222222222222", Status: 0, Desc: "成功!"}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"foo": "bar",
+		"nbf": time.Date(2022, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+	})
+	//密钥
+	var keyVal interface{}
+	keyVal = []byte("123456")
+	tokenString, err := token.SignedString(keyVal)
+	fmt.Println(tokenString, err)
+	return models.Auths{Username: userinfo.UserName, EmplyeeName: employee.EmpName, AuthsToken: tokenString, Status: 0, Desc: "成功!"}
 }
